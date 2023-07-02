77682e067a27156c3b6ac1dc6deff2f8a84af2a4
jvill171 <juanvillarreal64@yahoo.com>
2023-07-01 23:02:51 -0500

    Added companyFiltering method & tests for new method

--- helpers/sql.js
+++ helpers/sql.js
@@ -32,4 +32,41 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   };
 }
 
-module.exports = { sqlForPartialUpdate };
+/** Creates a string of filter conditions and an array of values to query companies table
+ * 
+ * colNames is a dictionary object:     { name: "col_name_in_DB", ... }
+ * dataToFilter is a dictionary object: { name: "value", ... }
+ * 
+ * Possible columns to filter by are:
+ *  name - using ILIKE for case-insesetive filtering of company names
+ *  emin - num_employees minimum
+ *  emax - num_employees maximum
+ * 
+ * Returns an object like:
+ *      {
+ *          filterCols: '"name" ILIKE $1 AND "num_employees"<=$2 AND "num_employees">=$3',
+ *          values: ['%value1%', 'value2', 'value3']
+ *      }
+*/
+
+function companyFiltering(colNames, dataToFilter){
+  const keys = Object.keys(colNames);
+
+  if(keys.includes("name")) dataToFilter.name = `%${dataToFilter.name}%`
+
+  const filterCols = keys.map((curCol, idx)=>{
+    let action;
+    if(keys[idx] === 'name') action = " ILIKE ";
+    else if(keys[idx] === 'emin') action = ">=";
+    else if(keys[idx] === 'emax') action = "<=";
+
+    return `"${colNames[curCol]}"${action}$${idx+1}`
+  })
+  
+  return {
+    filterCols: filterCols.join(" AND "),
+    values: Object.values(dataToFilter)
+  }
+}
+
+module.exports = { sqlForPartialUpdate, companyFiltering };
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,4 +1,4 @@
-const { sqlForPartialUpdate }= require("./sql")
+const { sqlForPartialUpdate, companyFiltering }= require("./sql")
 const { BadRequestError } = require("../expressError");
 
 
@@ -28,5 +28,74 @@ describe("sqlForPartialUpdate", ()=>{
         } catch(err){
             expect(err instanceof BadRequestError).toBeTruthy();
         }
+    })
+})
+
+describe("companyFiltering", ()=>{
+    // All cols
+    const cols1 = { name: 'name', emin: 'num_employees', emax: 'num_employees' }
+    const data1 = { name: 'test', emin: '1', emax: '100' }
+    // emin & emax only
+    const cols2 = { emin: 'num_employees', emax: 'num_employees' }
+    const data2 = { emin: '1', emax: '100' }
+    // name only
+    const cols3 = { name: 'name'}
+    const data3 = { name: 'test'}
+
+    test("works: All colums", ()=>{
+        let c = Object.assign({}, cols1);
+        let d = Object.assign({}, data1);
+        
+        const result = companyFiltering(c, d);
+        expect(result).toEqual({
+            filterCols: '"name" ILIKE $1 AND "num_employees">=$2 AND "num_employees"<=$3',
+            values: [ '%test%', '1', '100' ]
+          })
+    })
+    test("works: emin & emax only", ()=>{
+        let c = Object.assign({}, cols2);
+        let d = Object.assign({}, data2);
+        
+        const result = companyFiltering(c, d);
+        expect(result).toEqual({
+            filterCols: '"num_employees">=$1 AND "num_employees"<=$2',
+            values: [ '1', '100' ]
+          })
+    })
+    test("works: name only", ()=>{
+        let c = Object.assign({}, cols3);
+        let d = Object.assign({}, data3);
+        
+        const result = companyFiltering(c, d);
+        expect(result).toEqual({
+            filterCols: '"name" ILIKE $1',
+            values: [ '%test%' ]
+          })
+    })
+    test("works: no data - should never happen", ()=>{
+        const result = companyFiltering({}, {});
+        expect(result).toEqual({
+            filterCols: '',
+            values: []
+          })
+    })
+    test("works: more cols than data - should never happen", ()=>{
+        let c = Object.assign({}, cols1);
+        let d = Object.assign({}, data3);
+        
+        const result = companyFiltering(c, d);
+        expect(result).toEqual({
+            filterCols: '"name" ILIKE $1',
+            values: [ '%test%' ]
+        })
+    })
+    test("works: more data than cols - should never happen", ()=>{
+        let c = Object.assign({}, cols3);
+        let d = Object.assign({}, data1);
+        const result = companyFiltering(c, d);
+        expect(result).toEqual({
+            filterCols: '"name" ILIKE $1 AND "undefined">=$2 AND "undefined"<=$3',
+            values: [ '%test%', '1', '100' ]
+        })
     })
 })
